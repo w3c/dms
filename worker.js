c17f4029791c06832e4078d1c038a0d421e92eba
Nelson Solano <nelsonxsolano@gmail.com>
2020-02-13 15:11:26 -0800

    added documentation for _args and closure

--- worker.js
+++ worker.js
@@ -5,9 +5,17 @@ let debug_mode_ = false;
 const ERROR_PREFIX = "ERROR";
 const ERROR_PREFIX_LENGTH = ERROR_PREFIX.length;
 
+let runtimeReady= false;
+
 var Module = {
-  onRuntimeInitialized: function() {},
+  onRuntimeInitialized: function() {
+    // Set the runtimeReady flag to true, indicating that the WebAssembly
+    // module has been compiled and its components are ready to use.
+    runtimeReady = true;
+    console.log('Runtime is ready.');
+  },
   print: function(text) {
+    // console.log(text);
     if (text.substr(0, PLANNER_LOG_TAG_LENGTH) === PLANNER_LOG_TAG) {
       const msg = text.substr(PLANNER_LOG_TAG_LENGTH+1);
       if (msg.substr(0, ERROR_PREFIX_LENGTH) === ERROR_PREFIX && !debug_mode_) {
@@ -18,7 +26,7 @@ var Module = {
   }
 };
 
-importScripts('./dm.js');
+importScripts('dm.js');
 
 let inst = null;
 
@@ -38,17 +46,32 @@ function handleEvent(input, slots = {}) {
   inst.SetInformationState("_slots", String(JSON.stringify(slots)));
 }
 
-self.onmessage = msg => {
-  let msg_data = msg.data;
+function processWhenRuntimeReady(messageData) {
+  // Processing messages sent from "main" require calling functions from
+  // the WebAssembly module. Therefore we must wait until the WebAssembly
+  // module is fully initialized before processing any messages.
 
-  switch (msg_data.type) {
-    case 'start':
-      start_dm(msg_data.dependencies, msg_data.debug_mode);
-      break;
-    case 'handle-event':
-        handleEvent(msg_data.input, msg_data.slots);
-      break;
-    default:
-      throw new Error('Unknown message from main: ' + JSON.stringify(msg_data));
+  if (!runtimeReady) {
+    // The WebAssembly module has not been compiled yet. Try again in 100ms.
+    setTimeout(processWhenRuntimeReady, 100, messageData);
+  } else {
+    // The WebAssembly module has been compiled and its components are ready to
+    // use.
+    switch (messageData.type) {
+      case 'start':
+        start_dm(messageData.dependencies, messageData.debug_mode);
+        break;
+      case 'handle-event':
+        // console.log("handling event");
+        handleEvent(messageData.input, messageData.slots);
+        break;
+      default:
+        throw new Error('Unknown message from main: ' + JSON.stringify(messageData));
+    }
   }
+}
+
+self.onmessage = msg => {
+  let messageData = msg.data;
+  processWhenRuntimeReady(messageData);
 };
